52354b23ac44b9ab787851cd04edf6eff6c1b1e6
Vili Heikkilä <vili.heikkila@zalando.fi>
2022-12-15 21:35:35 +0200

    d4 done

--- app/src/main/kotlin/advent/of/kotlin/App.kt
+++ app/src/main/kotlin/advent/of/kotlin/App.kt
@@ -1,7 +1,7 @@
 package advent.of.kotlin
 
-import advent.of.kotlin.days.Day3
+import advent.of.kotlin.days.Day4
 
 fun main() {
-    Day3().part2()
+    Day4().part2()
 }
---
+++ app/src/main/kotlin/advent/of/kotlin/days/Day4.kt
@@ -0,0 +1,56 @@
+package advent.of.kotlin.days
+
+import advent.of.kotlin.utils.readInputFileByLines
+
+class Day4 {
+    class ElfPair(s: String) {
+        val sections: Pair<Set<Int>, Set<Int>>
+
+        init {
+            val split = s.split(",")
+            val elf1 = split[0].split("-")
+            val elf2 = split[1].split("-")
+            sections = Pair(
+                (elf1[0].toInt()..elf1[1].toInt()).toSet(),
+                (elf2[0].toInt()..elf2[1].toInt()).toSet()
+            )
+        }
+
+        override fun toString(): String {
+            return "${sections.first} - ${sections.second}"
+        }
+
+        fun eitherOneFullyContains(): Boolean {
+            return sections.first.containsAll(sections.second) ||
+                    sections.second.containsAll(sections.first)
+        }
+
+        fun overlapsAtAll(): Boolean {
+            return sections.first
+                .intersect(sections.second)
+                .isEmpty()
+                .not()
+        }
+    }
+
+    private fun parsePairs(raw: List<String>): List<ElfPair> {
+        return raw
+            .map { ElfPair(it) }
+    }
+
+    fun part1() {
+        val raw = readInputFileByLines("day4.txt")
+        val pairs = parsePairs(raw)
+        val total = pairs.count { it.eitherOneFullyContains() }
+        println(total)
+    }
+
+    fun part2() {
+        val raw = readInputFileByLines("day4.txt")
+        val pairs = parsePairs(raw)
+        val total = pairs.count { it.overlapsAtAll() }
+        println(total)
+
+    }
+
+}
