e4475fc5e0b1f91fddb74f71f5a7ad550fd7cea1
Vili Heikkilä <vili.heikkila@zalando.fi>
2022-12-25 19:00:32 +0200

    wrong answer

--- app/src/main/kotlin/advent/of/kotlin/days/Day13.kt
+++ app/src/main/kotlin/advent/of/kotlin/days/Day13.kt
@@ -1,10 +1,32 @@
 package advent.of.kotlin.days
 
-import advent.of.kotlin.utils.readInputFileByLines
+import advent.of.kotlin.utils.readFileAsString
 
 class Day13 {
 
-    open class Value
+    open class Value {
+        fun compare(comp: Value): Boolean {
+            if (this is Number && comp is Number) {
+                return  this.x <= comp.x
+            }
+
+            if (this is Vec && comp is Number) {
+                val wrapped = Vec(listOf(comp))
+                return this.compare(wrapped)
+            }
+
+            if (this is Number && comp is Vec) {
+                val wrapped = Vec(listOf(this))
+                return wrapped.compare(comp)
+            }
+
+            if (this is Vec && comp is Vec) {
+                return this.compare(comp)
+            }
+            
+            throw Exception("Never")
+        }
+    }
 
     class Number(val x: Int) : Value() {
         override fun toString(): String {
@@ -16,8 +38,53 @@ class Day13 {
         override fun toString(): String {
             return "$x"
         }
+
+        fun head(): Value {
+            return x.first()
+        }
+
+        fun rest(): Vec {
+            return Vec(x.drop(1))
+        }
+
+        fun compare(comp: Vec): Boolean {
+            if (this.x.isEmpty()) {
+                return true
+            }
+            
+            if (comp.x.isEmpty()) {
+                return false
+            }
+            
+            val thisHead = this.head()
+            val compHead = comp.head()
+            if (thisHead is Number && compHead is Number) {
+                if (thisHead.x < compHead.x) {
+                    return true
+                }
+                if (thisHead.x > compHead.x) {
+                    return false
+                }
+                
+                return thisHead.x == compHead.x && 
+                        this.rest().compare(comp.rest())
+            }
+            
+            if (thisHead is Vec && compHead is Number) {
+                val wrapped = Vec(listOf(compHead))
+                return thisHead.compare(wrapped)
+            }
+            
+            if (thisHead is Number && compHead is Vec) {
+                val wrapped = Vec(listOf(thisHead))
+                return wrapped.compare(compHead)
+            }
+
+            return thisHead.compare(compHead) &&
+                    this.rest().compare(comp.rest())
+        }
     }
-    
+
     class Cursor(val s: String) {
         var idx = 1
         private var nestCount = 1
@@ -31,17 +98,20 @@ class Day13 {
                     vec.add(helper.first)
                     idx += helper.second.dec()
                 }
+
                 s[idx] == ']' -> {
                     nestCount--
                     if (nestCount == 0) {
                         return Pair(Vec(vec), idx)
                     }
                 }
+
                 s[idx].isDigit() && s[idx.inc()].isDigit() -> {
                     val twoDigitNum = "${s[idx].digitToInt()}${s[idx.inc()].digitToInt()}".toInt()
                     vec.add(Number(twoDigitNum))
                     idx++
                 }
+
                 s[idx].isDigit() -> {
                     vec.add(Number(s[idx].digitToInt()))
                 }
@@ -51,11 +121,27 @@ class Day13 {
         }
     }
 
+    fun parsePacketPairs(raw: String): List<Pair<Vec, Vec>> {
+        val twoLines = raw.split("\n\n").map { it.split("\n") }
+        return twoLines.map {
+            Pair(
+                Vec(Cursor(it[0]).process().first.x),
+                Vec(Cursor(it[1]).process().first.x),
+            )
+        }
+    }
+
+
     fun part1() {
-        val lines = readInputFileByLines("trial.txt")
-        val practice = "[3,[10,5],[8,6]]"
-        val list = Cursor(practice).process().first
-        println(list.x)
-        
+        val raw = readFileAsString("trial.txt")
+        val pairs = parsePacketPairs(raw)
+
+        val result = pairs
+            .map { it.first.compare(it.second) }
+            .withIndex()
+            .filter { it.value }
+            .map { it.index.inc() }
+
+        println(result)
     }
 }
